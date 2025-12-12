34efd8fd3399118f175591e926bdb9b568681cf4
Brilliantony <brilly053@gmail.com>
2025-12-13 00:01:05 +0700

    upgrade

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [...nextCoreWebVitals, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
